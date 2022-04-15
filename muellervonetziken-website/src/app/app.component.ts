50da7f7fd7b62b9829604d0c9bfa9bdd2f8ab144
fabianmueller7 <fabian_mueller7@sluz.ch>
2022-04-16 00:35:17 +0200

    menu for phone

--- muellervonetziken-website/src/app/app.component.ts
+++ muellervonetziken-website/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'muellervonetziken-website';
+  @ViewChild("mySidebar") mySidebar!: ElementRef;
+
+  w3_open(){
+    if (this.mySidebar.nativeElement.style.display === 'block') {
+      this.mySidebar.nativeElement.style.display = 'none';
+    } else {
+      this.mySidebar.nativeElement.style.display = 'block';
+    }
+  }
+
+   w3_close() {
+    this.mySidebar.nativeElement.style.display = "none";
+  }
+
 }
